ea5574178f1e757ef66d5219b6ecd04ad44ab9ef
Qing Wang <qingwang@utexas.edu>
2016-04-25 03:23:07 -0500

    Initial Commit

---
+++ Driver.java
@@ -0,0 +1,13 @@
+package mastermind;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		//Need to do IO, just using this array to test preliminarily
+		char[] array = {'Y', 'R', 'B', 'O'};
+		
+		Game game = new Game(array, 12, 4);
+		
+	}
+
+}
---
+++ Game.java
@@ -0,0 +1,63 @@
+package mastermind;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Game {
+	
+	char[] code;   //Make by CodeMaker
+	int numTries;  //How many tries the CodeBreaker gets
+	int triesMade = 0;  //How many tries have been made by CodeBreaker
+	char[][] tries;  //The combinations of validCodes that have been guessed by the CodeBreaker
+	char[][] clues;  //Clues provided by the CodeMaker 
+	ArrayList<Character> validCodes = new ArrayList<Character>(Arrays.asList('B', 'G', 'O', 'P', 'R', 'Y'));
+	
+	//Constructor depends on how many tries the player gets (numTries) and how many orbs per code (length)
+	public Game (char[] code, int numTries, int length) {
+		this.numTries = numTries;
+		this.code = code;
+		tries = new char[numTries][length];
+		clues = new char[numTries][length];
+		code = new char[length];
+	}
+		
+	//Returns true if Code is in right format, else returns false and system outputs a message based on what failure
+	public boolean checkCode(char[] code) {
+		int counter = 0; 
+			
+		if(code.length != 4) {
+			if(code.length > 4) {
+				System.out.println("Code is too long.");
+			} else {
+				System.out.println("Code is too short.");
+			}
+			return false;
+		}
+		for(char c : code) {
+			if(!validCodes.contains(c)) {	
+				System.out.println(c + " is not a valid color choice.");
+			} else {
+				counter++;
+			}
+		}
+		if(counter < 4) {  //This means that one of the codes were invalid
+			return false;
+		}
+		return true;
+	}
+		
+	//True if it is a valid try by the CodeBreaker, false if it is not 
+	public boolean makeGuess(char[] code) {
+		if(!checkCode(code)) {
+			return false;
+		}
+		for(int i = 0; i < triesMade; i++) {     //Repeat guess
+			if(code == tries[i]) {      //QUESTION: Does this compare rows or columns? Need it to compare rows
+				return false;
+			}
+		}
+			
+		triesMade++;
+		return true;
+	}
+}
