1987aabd9391536023fe7b9a9a8cdb0002b32e84
Katelyn Ge <katelyn_ge@utexas.edu>
2016-04-27 23:32:21 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -14,8 +14,11 @@ public class Driver {
 		char[] input={};
 		Game game = new Game(array, 12, 4);
 		int choice = 0;
+		int gameswon = 0;
+		int gameslost = 0;
 		String answer="";
 		String result="";
+		String codemaker="";
 		boolean running = true;
 		boolean newGame = true;
 		boolean correctInput = false;
@@ -28,8 +31,27 @@ public class Driver {
 					running = (choice == JOptionPane.YES_OPTION);
 					newGame = false;
 					if (running) {
+					while(true){
 					answer = JOptionPane.showInputDialog("Insert number of maximum tries: ");
+					if (isNumber(answer)){
+						break;
+					}
+					else{
+						System.out.println("INVALID: insert an integer");
+					}
+					}
+					while (true){
 					result = JOptionPane.showInputDialog("Insert number of pegs: ");
+					if (isNumber(result)){
+						break;
+					}
+					else{
+						System.out.println("INVALID: insert an integer");
+					}
+					}
+					choice = JOptionPane.showConfirmDialog(null, "You have the option to create your own code for another player to guess. Click Yes if you would like to. Click No if you would like a random code generated for you. ",
+							null, JOptionPane.YES_NO_OPTION);
+					if (choice == JOptionPane.NO_OPTION){
 						System.out.println("Generating secret code..."+"\n");
 						game = new Game(array, Integer.parseInt(answer), Integer.parseInt(result));
 						game.makeNewGame();
@@ -37,6 +59,18 @@ public class Driver {
 						for (int i=0; i<game.code.length;i++){
 							System.out.print(game.code[i]);
 						}
+					}
+					else{
+						game = new Game (array, Integer.parseInt(answer), Integer.parseInt(result));
+						while(true){
+						codemaker = JOptionPane.showInputDialog("Input code (must be made out of B, G, O, P, R, Y): ");
+						array = codemaker.toCharArray();
+						if (game.checkCode(array)){
+							break;
+						}
+						}
+						game.code = array;
+					}
 					}
 				}
 				//make guesses
@@ -48,11 +82,12 @@ public class Driver {
 				}
 				while (!correctInput) {
 					System.out.println("What is your next guess?");
-					System.out.println("Type in the characters for your guess and press OK.");
+					System.out.println("Type in "+ (game.code.length) +  " characters for your guess and press OK.");
 					answer = JOptionPane.showInputDialog("Enter guess: ");
 					input = answer.toCharArray();
 					if (game.makeGuess(input)) {
 						correctInput = true;
+						System.out.print(answer+ "-->");
 						break;
 					}
 					else{
@@ -63,22 +98,20 @@ public class Driver {
 				correctInput = false;				//reset code check
 				System.out.println("\n" + game.showClue() + "\n");				//this isn't showing clues. why not?
 				
-				/*
-				if ((game.blackScore+game.whiteScore)==0){
-					result = "No pegs";
-				}
-				else{
-					result = game.blackScore + " black pegs, "+ game.whiteScore+ " white pegs";
-				}
-				System.out.println("\n"+answer+"--> Result: "+result+"\n");
-				*/
 				
-				
-				if (game.isGameOver()){
+				if (game.isGameOver() || game.gameWon){
+					if (game.gameWon){
+						gameswon++;
+					}
+					else{
+						gameslost++;
+					}
+					System.out.println("Games won: "+gameswon+ ", Games lost: "+gameslost);
 					choice = JOptionPane.showConfirmDialog(null,
 							"Would you like to play again?",
 							null, JOptionPane.YES_NO_OPTION);
 					running = (choice == JOptionPane.YES_OPTION);
+					newGame = true;
 				}
 			}
 		} catch (NullPointerException e) {
@@ -88,5 +121,17 @@ public class Driver {
 		}
 	}
 	
+	public static boolean isNumber(String str)  
+	{  
+	  try  
+	  {  
+	    int x = Integer.parseInt(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
+	}
 
 }
